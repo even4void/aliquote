221457d7f4f563d1a83bf05d642343994c9e10db
chl <chl@aliquote.org>
2023-01-25 12:10:24 +0100

    Minor fix

--- static/articles/scripts/org-setup-ggplot.r
+++ static/articles/scripts/org-setup-ggplot.r
@@ -1,5 +1,5 @@
 ## Little R script that comes along org-setup.org
-## http://aliquote.org/articles/notebooks/org-setup.pdf
+## http://aliquote.org/pub/org-setup.pdf
 ## Time-stamp: <2022-04-29 21:55:38 chl>
 library(ggplot2)
 library(directlabels)
@@ -18,8 +18,8 @@ d <- expand.grid(lwt = seq(40, 100), race = factor(levels(birthwt$race)))
 d$yhat <- predict(m, d, type = "response")
 
 p <- ggplot(data = d, aes(x = lwt, y = yhat, color = race)) +
-       geom_line(aes(group = race), size = 1) +
-       scale_color_brewer(palette = "Set2") +
-       guides(color = FALSE) +
-       labs(x = "Mother weight (kg)", y = "Pr(low = 1)", caption = "Predicted response curves")
+  geom_line(aes(group = race), size = 1) +
+  scale_color_brewer(palette = "Set2") +
+  guides(color = FALSE) +
+  labs(x = "Mother weight (kg)", y = "Pr(low = 1)", caption = "Predicted response curves")
 direct.label(p + aes(label = race), method = "smart.grid")
